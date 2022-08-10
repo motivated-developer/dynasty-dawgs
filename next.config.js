aab91ac62ace02e3e98c46bf7b8133f3c0de89d6
Oreo59 <rcharnoky@gmail.com>
2022-08-11 01:16:08 +0200

    Redirect to the league overview page from the default index next page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/leagueOverview',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
